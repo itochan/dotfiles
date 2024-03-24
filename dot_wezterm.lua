b03ac7b4f2e964248ae99ee728c315abec6f10f0
Kazunori Jo <519604+itochan@users.noreply.github.com>
2024-03-24 20:40:50 +0900

    Add wezterm configuration

---
+++ dot_wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.font = wezterm.font_with_fallback {
+  'UDEV Gothic 35',
+  'JetBrains Mono',
+}
+config.font_size = 16.0
+
+config.initial_cols = 120
+config.initial_rows = 36
+
+return config
